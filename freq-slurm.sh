bca1d4026f096c6363a0266377f5968e1914897c
AWS ParallelCluster user <ec2-user@ip-10-0-14-188.ap-southeast-1.compute.internal>
2021-11-08 04:49:20 +0000

    edits

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -13,27 +13,4 @@
 #SBATCH -e freq-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-python -u -c "import PyHipp as pyh; \
-import time; \
-pyh.RPLLFP(saveLevel=1); \
-pyh.FreqSpectrum(saveLevel=1); \
-print(time.localtime());"
-
-/data/miniconda3/bin/conda init
-source ~/.bashrc
-envarg=`/data/src/PyHipp/envlist.py`
-conda activate $envarg
-
-python -u -c "import PyHipp as pyh; \
-import DataProcessingTools as DPT; \
-import time; \
-pyh.RPLHighPass(saveLevel = 1); \
-from PyHipp import mountain_batch; \
-mountain_batch.mountain_batch(); \
-from PyHipp import export_mountain_cells; \
-export_mountain_cells.export_mountain_cells(); \
-pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-print(time.localtime());"
-
-conda deactivate
-/data/src/PyHipp/envlist.py $envarg
+python -u -c "import PyHipp as pyh; pyh.FreqSpectrum(saveLevel=1); pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);"
