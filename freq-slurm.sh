054a24873be40016a3716a7a5804eb2c776620d4
AWS ParallelCluster user <ec2-user@ip-10-0-14-188.ap-southeast-1.compute.internal>
2021-11-08 04:26:58 +0000

    lab9

---
+++ freq-slurm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --cpus-per-task=1	# number of processors per task
+#SBATCH -J "freq"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o freq-slurm.%N.%j.out # STDOUT
+#SBATCH -e freq-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; \
+import time; \
+pyh.RPLLFP(saveLevel=1); \
+pyh.FreqSpectrum(saveLevel=1); \
+print(time.localtime());"
+
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
+python -u -c "import PyHipp as pyh; \
+import DataProcessingTools as DPT; \
+import time; \
+pyh.RPLHighPass(saveLevel = 1); \
+from PyHipp import mountain_batch; \
+mountain_batch.mountain_batch(); \
+from PyHipp import export_mountain_cells; \
+export_mountain_cells.export_mountain_cells(); \
+pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
+print(time.localtime());"
+
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
